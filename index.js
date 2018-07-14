7099590f4d08b09c4f2c31d442902cd23496ba62
Stanislas Ormières <laruiss@gmail.com>
2018-07-14 15:54:59 +0200

    Get the request method

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ const server = http.createServer((req, res) => {
   const path = parsedUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
+  // Get the method
+  const method = req.method.toLowerCase()
+
   // Send the response
   res.end("Hello World!\n")
 
   // Log the request path
-  console.log(trimmedPath)
+  console.log(method, '"' + trimmedPath + '"')
 })
 
 // Start listen on 3000
