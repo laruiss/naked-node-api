7577427efe9f7aa5c049cb05d886effd31784e49
Stanislas Ormières <laruiss@gmail.com>
2018-07-14 15:44:39 +0200

    Get the requested path

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const http = require('http')
+const http = require("http")
+const url = require("url")
 
 const port = 3000
 
 // Respond to all request with a string
-const server = http.createServer((req, res) => (
-    res.end('Hello World!\n')
-))
+const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true)
+  
+  // Get the path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Send the response
+  res.end("Hello World!\n")
+
+  // Log the request path
+  console.log(trimmedPath)
+})
 
 // Start listen on 3000
 server.listen(port, () => {
-    console.log('Listening on 3000')
+  console.log("Listening on 3000")
 })
